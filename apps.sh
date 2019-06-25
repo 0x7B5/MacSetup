90ed26224656ca7a872255ed60ad874d59f051e9
vlad-munteanu <vladcmunteanu@gmail.com>
2019-06-25 18:15:40 -0400

    Added apps.sh.

---
+++ apps.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#Utility to install apps 
+echo "Installing mas (tool to install apps)"
+brew install mas-cli/tap/mas
+
+#Firefox
+echo "Installing Firefox" 
+brew cask install firefox
+
+#iTerm 
+echo "Installing iTerm" 
+brew cask install iterm2
+
+#Sublime Text 
+echo "Installing Sublime Text" 
+brew cask install sublime-text
+
+#Visual Studio Code
+echo "Installing Visual Studio" 
+brew cask install visual-studio-code
+
+#The Unarchiver
+echo "The Unarchiver"
+mas install 425424353
+
+#Spotify 
+echo "Spotify" 
+brew cask install spotify
+
+#Pages
+echo "Pages"
+mas install 409201541
+
+#Xcode 
+echo "Xcode" 
+mas install 497799835
+
+echo "Install Window Tidy" 
+echo "Install Impactor" 
+echo "Install IDA Pro" 
+echo "Install Hopper" 
+echo "Install Photoshop" 
+echo "Install uTorrent" 
+echo "Install Clean My Mac" 
+
+
