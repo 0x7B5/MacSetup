b898078aaaeb71aee19adc977d09bc1a8302c7d3
Vlad Munteanu <vladcmunteanu@gmail.com>
2019-08-04 15:54:05 -0400

    Removed pinentry.

--- initial.sh
+++ initial.sh
@@ -46,8 +46,6 @@ brew install gnupg
 echo "Generating new key for github"
 brew upgrade gnupg
 brew link --overwrite gnupg
-brew install pinentry-mac
-echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
 killall gpg-agent
 
 gpg2 --full-generate-key
